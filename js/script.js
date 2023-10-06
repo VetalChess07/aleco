e3ebb8fe6ea292237953a4d2342a5d41a7daadbf
VetalChess07 <103288446+VetalChess07@users.noreply.github.com>
2023-10-06 20:28:44 +0300

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,32 +1,23 @@
 import { CountUp } from "./countUp.min.js";
-console.log(CountUp);
 
 const options = {
   duration: 6.7,
 };
 
 let statistics__instagram = new CountUp("statistics__instagram", 150, {
-  enableScrollSpy: true,
+  duration: 10.0,
 });
-let statistics__telegram = new CountUp("statistics__telegram", 105, {
-  enableScrollSpy: true,
-});
+let statistics__telegram = new CountUp("statistics__telegram", 105, {});
 
 let statistics__vk = new CountUp("statistics__vk", 12, {
-  enableScrollSpy: true,
+  duration: 10.0,
 });
 let statistics__youtube = new CountUp("statistics__youtube", 5, {
-  enableScrollSpy: true,
+  duration: 10.0,
 });
 let statistics__result = new CountUp("statistics__result", 280, {
-  enableScrollSpy: true,
+  duration: 10.0,
 });
-
-statistics__instagram.handleScroll();
-statistics__vk.handleScroll();
-statistics__youtube.handleScroll();
-statistics__telegram.handleScroll();
-statistics__result.handleScroll();
 
 if (!statistics__instagram.error) {
   statistics__instagram.start();
@@ -38,4 +29,4 @@ if (!statistics__instagram.error) {
   console.error(statistics__instagram.error);
 }
 
-AOS.init();
+// AOS.init();
